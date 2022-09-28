83772d04563fb7e356c13c8abb9f09a87d11c5f4
Jake Gillenwater <JakeGill70@gmail.com>
2022-09-28 17:29:23 -0400

    Init

---
+++ SimulationLogic/Models/GuiController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimulationLogic
+{
+    public class GuiController : IGuiController
+    {
+        /// <summary>
+        /// Implements <see cref="IGuiController.UpdateUI(Supermarket, string[], List{string}[], ISupermarketStatistics)"/>
+        /// </summary>
+        public void UpdateUI(Supermarket supermarket, string[] queueLabels, List<string>[] queueOfCustomers, ISupermarketStatistics supermarketStatistics)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ SimulationLogic/Models/Supermarket.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimulationLogic
+{
+    public class Supermarket : ISupermarket
+    {
+
+        //Attributes		
+
+        public void Run()
+        {
+            //Your code to run the simulation goes here
+            //...
+        }
+    }
+}
